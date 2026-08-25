57eb1c715bb90e62e0d6453d83c0ff3523bbd790
Ramon Silva <ramonsillva70@gmail.com>
2026-08-25 15:27:54 -0300

    Adiciona e-mail na seção de Contato

--- src/components/sections/Contact.vue
+++ src/components/sections/Contact.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 const contacts = [
   {
+    name: 'E-mail',
+    value: 'ramonsillva70@gmail.com',
+    url: 'mailto:ramonsillva70@gmail.com',
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>'
+  },
+  {
     name: 'WhatsApp / Telefone',
     value: '(94) 99177-7860',
     url: 'https://wa.me/5594991777860',
